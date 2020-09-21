09179b768c43be9ce7acb1b9ba0c2cf10a580149
hds536jhmk <hds536jhmk@gmail.com>
2020-09-21 14:13:37 +0200

    Documented utils.js

--- utils.js
+++ utils.js
@@ -1,8 +1,9 @@
 
 /**
- * 
- * @param {String} str
- * @param  {...String} formats
+ * Formats the specified string replacing all {n} where n is a number with the corresponding argument
+ * @param {String} str - The string to format
+ * @param {...String} formats - All formats
+ * @returns {String} The formatted string
  */
 exports.formatString = (str, ...formats) => {
     formats.forEach((value, i) => {
